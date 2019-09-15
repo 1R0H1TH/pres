181611b29042044e6f6da56c2e576d0f519bbcb3
Rohith Ingilela <48469639+iRohith@users.noreply.github.com>
2019-09-15 07:54:31 +0000

    Update py2c_3d_9.c

--- py2c_3d_9.c
+++ py2c_3d_9.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -7,21 +6,19 @@
 typedef double** Matrix; typedef double** Vector;
 
 Matrix meshgrid(int len, int start, int stepX, int stepY);
-void saveMat(Matrix mat, char*str, int m, int n);
 Matrix *createPlane(Vector n, double c, int meshLen);
 Vector foot(Vector n, double c_, Vector p);
+Vector createVec(double x,double y,double z);
 
 int main(){
 	
 	// Assigning point p = (1 -2 4)^T, x = (1 2 2)^T (x is point on plane)
-	Vector p = createMat(3,1); Vector x = createMat(3,1);
-	*p[0] = 1; *p[1] = -2; *p[2] = 4;
-	*x[0] = 1; *x[1] =  2; *x[2] = 2;
+	Vector p = createVec(1,-2,4);
+	Vector x = createVec(1,2,2);
 	
 	// Assigning normal vectors of given planes
-	Vector n1 = createMat(3,1); Vector n2 = createMat(3,1);
-	*n1[0] = 1; *n1[1] = -1; *n1[2] = 2;
-	*n2[0] = 2; *n2[1] = -2; *n2[2] = 1;
+	Vector n1 = createVec(1,-1,2);
+	Vector n2 = createVec(2,-2,1);
 	double c1 = 3; double c2 = -12;
 	
 	// Matrix for cross product
@@ -41,28 +38,28 @@ int main(){
 	printf("Plane equation : (%d %d %d)x = %d\n", (int)*n[0], (int)*n[1], (int)*n[2], (int)c);
 	printf("Distace = %lf\n\n",dist);
 	
-	Vector foo = foot(n, c, p);
-	print(foo,3,1);
+	Vector foot_ = foot(n, c, p);
 	
 	int meshLen = 10;
 	
 	Matrix *plane1 = createPlane(n1, c1, meshLen);
-	saveMat(plane1[0], "data/meshX1.dat", meshLen, meshLen);
-	saveMat(plane1[1], "data/meshY1.dat", meshLen, meshLen);
-	saveMat(plane1[2], "data/meshZ1.dat", meshLen, meshLen);
+	savetxt(plane1[0], "data/meshX1.dat", meshLen, meshLen);
+	savetxt(plane1[1], "data/meshY1.dat", meshLen, meshLen);
+	savetxt(plane1[2], "data/meshZ1.dat", meshLen, meshLen);
 	
 	Matrix *plane2 = createPlane(n2, c2, meshLen);
-	saveMat(plane2[0], "data/meshX2.dat", meshLen, meshLen);
-	saveMat(plane2[1], "data/meshY2.dat", meshLen, meshLen);
-	saveMat(plane2[2], "data/meshZ2.dat", meshLen, meshLen);
+	savetxt(plane2[0], "data/meshX2.dat", meshLen, meshLen);
+	savetxt(plane2[1], "data/meshY2.dat", meshLen, meshLen);
+	savetxt(plane2[2], "data/meshZ2.dat", meshLen, meshLen);
 	
 	*n[1] += *n[2]; *n[2] = *n[1] - *n[2]; *n[1] -= *n[2]; meshLen += 10;
 	
 	Matrix *plane3 = createPlane(n, c, meshLen);
-	saveMat(plane3[0], "data/meshX3.dat", meshLen, meshLen);
-	saveMat(plane3[1], "data/meshZ3.dat", meshLen, meshLen);
-	saveMat(plane3[2], "data/meshY3.dat", meshLen, meshLen);
+	savetxt(plane3[0], "data/meshX3.dat", meshLen, meshLen);
+	savetxt(plane3[1], "data/meshZ3.dat", meshLen, meshLen);
+	savetxt(plane3[2], "data/meshY3.dat", meshLen, meshLen);
 	
+	savetxt(p,"data/P.dat",3,1); savetxt(foot_,"data/foot.dat",3,1);
 	// Disposing resources
 	free(p); free(x); free(n1); free(n2); free(n1o); free(n); free(plane1); free(plane2); free(plane3);
 	return 0;
@@ -88,18 +85,6 @@ Matrix *createPlane(Vector n, double c, int meshLen){
 	return ret;
 }
 
-void saveMat(Matrix mat, char *str, int m, int n){
-	FILE *fp;
-	fp = fopen(str,"w");
-	for (int i=0; i < m; i++){
-		for (int j=0; j < n; j++){
-			fprintf(fp, "%lf ", mat[i][j]);
-		}
-		fprintf(fp, "\n");
-	}
-	fclose(fp);
-}
-
 Vector foot(Vector n, double c_, Vector p){
 	double a=*n[0]; double b=*n[1]; double c=*n[2]; double d = -c_;
 	double x1=*p[0]; double y1=*p[1]; double z1=*p[2];
@@ -109,4 +94,9 @@ Vector foot(Vector n, double c_, Vector p){
     *ret[1] = b * k + y1;
     *ret[2] = c * k + z1;
     return ret;
+}
+Vector createVec(double x,double y,double z){
+	Vector vec = createMat(3,1);
+	*vec[0]=x; *vec[1]=y; *vec[2]=z;
+	return vec;
 }
